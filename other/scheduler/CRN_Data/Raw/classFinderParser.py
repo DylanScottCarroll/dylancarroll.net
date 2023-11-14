2c86de7bddc82e0c57c8989246ad64637db9e1a6
Dylan Carroll <dyscarroll@gmail.com>
2023-11-14 11:31:54 -0800

    Fixed winter 2024 data

--- other/scheduler/CRN_Data/Raw/classFinderParser.py
+++ other/scheduler/CRN_Data/Raw/classFinderParser.py
@@ -1,6 +1,6 @@
 from ctypes import ArgumentError
 from bs4 import BeautifulSoup
-import re, sys
+import re, sys, os, time
 
 DEFAULT_COLOR = 0
 DEFAULT_SHADE = 0
@@ -28,7 +28,8 @@ def extractDatas(tr):
     except:
         print()
         print(currentTR.contents[6])
-        input()
+        input("Hit enter to continue", end="")
+        print(end="\r")
     
     credits = ""
 
@@ -131,6 +132,20 @@ def tupleToSaveString(dataTuple):
 
     return string
 
+def print_progress(i, n):
+    completion = (i / n)
+    terminal_size = os.get_terminal_size().columns
+    width = terminal_size - 7 - 3
+    bar_width = int(width*completion)
+
+    terminator = "\n" if i == n else "\r"
+
+    print(f"{completion*100:6.2f}% [" + ("#"*bar_width) + (" "*(width-bar_width)) + "]", end=terminator)
+
+    
+
+
+
 if __name__ == "__main__":
     #Read the html text from the file
     if len(sys.argv) != 2:
@@ -155,18 +170,22 @@ if __name__ == "__main__":
         scratchpad_form.decompose()
 
     selTermInputs = soup.findAll("input", attrs={"name": "sel_term"})
+    selTermCount = len(selTermInputs)
 
     saveString = ""
-    for selTermInput in selTermInputs:
+    for i, selTermInput in enumerate(selTermInputs):
+        print_progress(i, selTermCount)
 
         currentTR = selTermInput.parent
 
         datas = extractDatas(currentTR)
         saveString += tupleToSaveString(datas) + "\n"
+    print_progress(selTermCount, selTermCount)
 
     f = open("output.txt", "w")
     f.write(saveString)
     f.close()
 
   
+
 
